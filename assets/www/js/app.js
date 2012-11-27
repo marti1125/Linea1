142ad3d1e2ff33afbfc5a238a7f08de87547625f
Willy Aguirre <willy@willy-K52Dr.(none)>
2012-11-27 09:39:09 -0500

    actulizando las ubicaciones

--- assets/www/js/app.js
+++ assets/www/js/app.js
@@ -1,7 +1,10 @@
 // Wait for Cordova to load
     //
+    var map;
+    var infowindow;
+
     function mostrar() {
-        document.addEventListener("deviceready", onDeviceReady, false);
+        document.addEventListener("deviceready", onDeviceReady, false);            
     }    
    
 
@@ -13,20 +16,60 @@
 
     // onSuccess Geolocation
     //
-    function onSuccess(position) {
-        var element = document.getElementById('geolocation');
-        element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
-                            'Longitude: '          + position.coords.longitude             + '<br />' +
-                            'Altitude: '           + position.coords.altitude              + '<br />' +
-                            'Accuracy: '           + position.coords.accuracy              + '<br />' +
-                            'Altitude Accuracy: '  + position.coords.altitudeAccuracy      + '<br />' +
-                            'Heading: '            + position.coords.heading               + '<br />' +
-                            'Speed: '              + position.coords.speed                 + '<br />' +
-                            'Timestamp: '          +                                   position.timestamp          + '<br />';
-        var latitud = position.coords.latitude;
-        var longitud = position.coords.longitude;
+    function onSuccess(position) {        
 
-        var ubicacion = new google.maps.LatLng(latitud, longitud);
+        //var element = document.getElementById('geolocation');
+        //element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
+                           // 'Longitude: '          + position.coords.longitude             + '<br />' +
+                            //'Altitude: '           + position.coords.altitude              + '<br />' +
+                            //'Accuracy: '           + position.coords.accuracy              + '<br />' +
+                            //'Altitude Accuracy: '  + position.coords.altitudeAccuracy      + '<br />' +
+                            //'Heading: '            + position.coords.heading               + '<br />' +
+                            //'Speed: '              + position.coords.speed                 + '<br />' +
+                            //'Timestamp: '          + position.timestamp                    + '<br />';
+        
+        var latitud = position.coords.latitude;
+        var longitud = position.coords.longitude; 
+
+        
+
+        var pyrmont = new google.maps.LatLng(latitud, longitud);
+
+        map = new google.maps.Map(document.getElementById('miestacion'), {
+          mapTypeId: google.maps.MapTypeId.ROADMAP,
+          center: pyrmont,
+          zoom: 14
+        });
+
+        var yo = new google.maps.LatLng(latitud, longitud);
+        var msgyo = 'Mi ubicación';
+
+        var marker = new google.maps.Marker({
+            position: yo,
+            map: map
+        }); 
+
+        var infowindowyo = new google.maps.InfoWindow({
+            content: msgyo
+        });
+
+        google.maps.event.addListener(marker, 'click', function () {
+            infowindowyo.open(map, marker);
+        });
+        
+
+        var request = {
+          location: pyrmont,
+          radius: 3000,
+          //types: ['train_station'] Linea1
+          types: ['subway_station']
+          //types: ['political']
+        };
+        infowindow = new google.maps.InfoWindow();
+        var service = new google.maps.places.PlacesService(map);
+        service.nearbySearch(request, callback);     
+
+        /*var ubicacion = new google.maps.LatLng(latitud, longitud);
 
         var mapOptions = {
 
@@ -41,7 +84,7 @@
         var marker = new google.maps.Marker({
             position: ubicacion,
             map: ubi
-        });
+        });*/
 
     }
 
@@ -51,3 +94,35 @@
         alert('code: '    + error.code    + '\n' +
                 'message: ' + error.message + '\n');
     }
+
+    //// mi ubicación...
+
+    
+
+    function initialize() { 
+
+        
+      }
+
+      function callback(results, status) {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+          for (var i = 0; i < results.length; i++) {
+            createMarker(results[i]);
+          }
+        }
+      }
+
+      function createMarker(place) {
+        var placeLoc = place.geometry.location;
+        var marker = new google.maps.Marker({
+          map: map,
+          position: place.geometry.location
+        });
+
+        google.maps.event.addListener(marker, 'click', function() {
+          infowindow.setContent(place.name);
+          infowindow.open(map, this);
+        });
+      }
+
+      google.maps.event.addDomListener(window, 'load', onSuccess);
