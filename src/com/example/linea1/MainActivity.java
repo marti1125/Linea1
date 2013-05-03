8020660730582e86a057b8112d520fc59dd1acb2
Willy Aguirre <marti1003@hotmail.com>
2013-05-03 00:15:02 -0500

    add asssets and update cordova

--- src/com/example/linea1/MainActivity.java
+++ src/com/example/linea1/MainActivity.java
@@ -1,22 +1,15 @@
 package com.example.linea1;
 
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import org.apache.cordova.*;
 
-public class MainActivity extends Activity {
+public class MainActivity extends DroidGap {
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		super.loadUrl("file:///android_asset/www/index.html");
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
+	
 }
